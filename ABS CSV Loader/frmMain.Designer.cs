2362e8ab8ed9821bf96b9499a04287fbfd0edd5e
Perera <pilperera@yahoo.com>
2014-04-19 17:10:07 +1000

    Base upload
    Base upload - working version

---
+++ ABS CSV Loader/frmMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ABS_CSV_Loader
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dlgFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.lstPending = new System.Windows.Forms.ListBox();
+            this.lstCompleted = new System.Windows.Forms.ListBox();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.lblDBServer = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.lblDataBase = new System.Windows.Forms.Label();
+            this.txtUid = new System.Windows.Forms.TextBox();
+            this.lblUid = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            resources.ApplyResources(this.btnOpen, "btnOpen");
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnExit
+            // 
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.Name = "btnExit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dlgFolderBrowser
+            // 
+            this.dlgFolderBrowser.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // lstPending
+            // 
+            this.lstPending.FormattingEnabled = true;
+            resources.ApplyResources(this.lstPending, "lstPending");
+            this.lstPending.Name = "lstPending";
+            this.lstPending.SelectedIndexChanged += new System.EventHandler(this.lstPending_SelectedIndexChanged);
+            // 
+            // lstCompleted
+            // 
+            this.lstCompleted.FormattingEnabled = true;
+            resources.ApplyResources(this.lstCompleted, "lstCompleted");
+            this.lstCompleted.Name = "lstCompleted";
+            this.lstCompleted.SelectedIndexChanged += new System.EventHandler(this.lstCompleted_SelectedIndexChanged);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.txtFolder, "txtFolder");
+            this.txtFolder.Name = "txtFolder";
+            // 
+            // txtCount
+            // 
+            this.txtCount.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.txtCount, "txtCount");
+            this.txtCount.Name = "txtCount";
+            // 
+            // txtFileName
+            // 
+            resources.ApplyResources(this.txtFileName, "txtFileName");
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            // 
+            // btnProcess
+            // 
+            resources.ApplyResources(this.btnProcess, "btnProcess");
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // txtServer
+            // 
+            resources.ApplyResources(this.txtServer, "txtServer");
+            this.txtServer.Name = "txtServer";
+            // 
+            // lblDBServer
+            // 
+            resources.ApplyResources(this.lblDBServer, "lblDBServer");
+            this.lblDBServer.Name = "lblDBServer";
+            // 
+            // txtDatabase
+            // 
+            resources.ApplyResources(this.txtDatabase, "txtDatabase");
+            this.txtDatabase.Name = "txtDatabase";
+            // 
+            // lblDataBase
+            // 
+            resources.ApplyResources(this.lblDataBase, "lblDataBase");
+            this.lblDataBase.Name = "lblDataBase";
+            // 
+            // txtUid
+            // 
+            resources.ApplyResources(this.txtUid, "txtUid");
+            this.txtUid.Name = "txtUid";
+            // 
+            // lblUid
+            // 
+            resources.ApplyResources(this.lblUid, "lblUid");
+            this.lblUid.Name = "lblUid";
+            // 
+            // txtPwd
+            // 
+            resources.ApplyResources(this.txtPwd, "txtPwd");
+            this.txtPwd.Name = "txtPwd";
+            // 
+            // lblPwd
+            // 
+            resources.ApplyResources(this.lblPwd, "lblPwd");
+            this.lblPwd.Name = "lblPwd";
+            // 
+            // frmMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.lblUid);
+            this.Controls.Add(this.lblDataBase);
+            this.Controls.Add(this.lblDBServer);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtUid);
+            this.Controls.Add(this.txtDatabase);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.lstCompleted);
+            this.Controls.Add(this.lstPending);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolderBrowser;
+        private System.Windows.Forms.ListBox lstPending;
+        private System.Windows.Forms.ListBox lstCompleted;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label lblDBServer;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label lblDataBase;
+        private System.Windows.Forms.TextBox txtUid;
+        private System.Windows.Forms.Label lblUid;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label lblPwd;
+    }
+}
+
---
+++ ABS CSV Loader/frmMain.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace ABS_CSV_Loader
+{
+    public partial class frmMain : Form
+    {
+        public int numFiles = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            lstCompleted.Items.Clear();
+            lstPending.Items.Clear();
+            numFiles = 0;
+            refreshcount();
+            dlgFolderBrowser.ShowDialog();
+            txtFolder.Text = dlgFolderBrowser.SelectedPath;
+            getdirectory(dlgFolderBrowser.SelectedPath);
+        }
+
+        private void getdirectory(string strPath)
+        {
+            DirectoryInfo directoryInfo= new DirectoryInfo(strPath); 
+            if(directoryInfo.Exists)
+            {
+                try
+                {
+                    FileInfo[] fileInfo = directoryInfo.GetFiles("*.csv");
+                    foreach (FileInfo file in fileInfo)
+                    {
+                        lstPending.Items.Add(file.FullName);
+                        numFiles++;
+                        refreshcount();
+                    }                   
+                }catch(Exception ex)
+                {
+                    txtFileName.Text = ex.Message;
+                    txtFileName.Refresh();
+                }
+                try
+                {
+                    DirectoryInfo[] subdirectoryInfo = directoryInfo.GetDirectories();
+                    foreach (DirectoryInfo subDirectory in subdirectoryInfo)
+                    {
+                        getdirectory(subDirectory.FullName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    txtFileName.Text = ex.Message;
+                    txtFileName.Refresh();
+                }
+            }                      
+        }
+
+        private void refreshcount()
+        {
+            txtCount.Text = numFiles.ToString() + " files in queue";
+            this.Refresh();
+        }
+
+        private void lstPending_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtFileName.Text = lstPending.SelectedItem.ToString();
+        }
+
+        private void lstCompleted_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtFileName.Text = lstCompleted.SelectedItem.ToString();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            var items = new System.Collections.ArrayList(lstPending.Items);
+            foreach (var item in items)
+            {
+                string strItemString = item.ToString();
+                string strPath = strItemString.Substring(0, strItemString.LastIndexOf("\\")) + "\\";
+                string strFileName = strItemString.Substring(strItemString.LastIndexOf("\\") + 1);
+                processfile(strPath, strFileName);
+                numFiles--;
+                refreshcount();
+                lstCompleted.Items.Add(item);
+                lstCompleted.Refresh();
+                lstPending.Items.Remove(item);
+                lstPending.Refresh();
+            }
+        }
+
+        private void processfile(string strPath, string strFileName)
+        {
+            txtFileName.Text = "Processing " + strFileName;
+            this.Refresh();
+            
+            string strTableName = "tbl" + strFileName.Replace(".csv","");
+
+
+            string strConnection = "Server=" + txtServer.Text.Trim() + " ;Database=" + txtDatabase.Text.Trim() + ";Uid=" + txtUid.Text.Trim() +";Pwd=" + txtPwd.Text.Trim();
+            MySqlConnection conn = new MySqlConnection(strConnection);
+            MySqlCommand cmd;
+            conn.Open();
+            try
+            {
+                cmd = conn.CreateCommand();
+                string line;
+                int counter = 0;
+                int numcols = 0;
+
+                System.IO.StreamReader file = new System.IO.StreamReader(strPath + strFileName);
+                while ((line = file.ReadLine()) != null)
+                {
+                    counter++;
+                    string SQLCommand = "";
+                    if (counter == 1)
+                    {
+                        string[] arrHeader = line.Split(',');
+                        numcols = arrHeader.Length;
+
+                        /*Create target table*/
+                        txtFileName.Text = "Creating target table " + strTableName;
+                        this.Refresh();
+
+                        SQLCommand = "create table if not exists " + strTableName + " (";
+                        for (int i = 0; i < numcols; i++)
+                        {
+                            if (i != 0) { SQLCommand = SQLCommand + ","; }
+                            SQLCommand = SQLCommand + "A" + i.ToString() + " text";
+                        }
+                        SQLCommand = SQLCommand + ");";
+                        cmd.CommandText = SQLCommand;
+                        cmd.ExecuteNonQuery();
+
+                        /*Add Metadata*/
+                        txtFileName.Text = "Udating meta info ... ";
+                        this.Refresh();
+
+
+                        SQLCommand = "delete from tblMeta where tblName = '" + strTableName + "' ;";
+                        cmd.CommandText = SQLCommand;
+                        cmd.ExecuteNonQuery();
+
+                        for (int i = 0; i < numcols; i++)
+                        {
+                            SQLCommand = "insert into tblMeta (tblName,tblfield,tblFieldDesc) values ('" + strTableName + "','" + "A" + i.ToString() + "','" + arrHeader[i] + "');";
+                            cmd.CommandText = SQLCommand;
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        txtFileName.Text = "Purging " + strTableName;
+                        this.Refresh();
+
+                        /*Purge target table*/
+                        SQLCommand = "delete from " + strTableName + ";";
+                        cmd.CommandText = SQLCommand;
+                        cmd.ExecuteNonQuery();
+                    }
+                    else 
+                    {
+                        txtFileName.Text = "Importing data into " + strTableName + " - " + counter.ToString() + " rows processed";
+                        this.Refresh();
+
+                        string[] arrHeader = line.Split(',');
+                        if (arrHeader.Length == numcols) 
+                        {
+                            /*insert row*/
+                            SQLCommand = "Insert into " + strTableName + " values (";
+                            for (int i = 0; i < numcols; i++)
+                            {
+                                if (i != 0) { SQLCommand = SQLCommand + ","; }
+                                SQLCommand = SQLCommand + "'" + arrHeader[i] +"'" ;
+                            }
+                            SQLCommand = SQLCommand + ");";
+                            cmd.CommandText = SQLCommand;
+                            cmd.ExecuteNonQuery();
+                        }
+                    
+                    }
+
+                }
+                file.Close();
+                txtFileName.Text = "Finished processing " + strFileName;
+                this.Refresh();
+            }
+            catch (Exception ex)
+            {
+                txtFileName.Text = ex.Message;
+                this.Refresh();
+            }
+            finally 
+            {
+                txtFileName.Text = "";
+                this.Refresh();
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+
+    }
+}
+
